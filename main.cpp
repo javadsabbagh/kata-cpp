f1e53815eb96514ac7c57a6024d843463957dc73
javad <j.sabbagh89@gmail.com>
2017-08-17 19:54:24 +0430

    initialize maps project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    map<int, string> myMap;
+    cout << myMap.size() << endl;
+            cout << "Hello World!" << endl;
+    return 0;
+}
